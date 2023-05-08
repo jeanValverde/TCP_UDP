cf7a10803f0004736717f2ef8071ba833c988d66
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2023-05-07 20:32:39 -0400

    Update main.go

--- BOT_GATO/main.go
+++ BOT_GATO/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+const (
+	IP         = "127.0.0.1"
+	HOST       = 8484
+	PROTOCOLO  = "udp"
+	DISPONIBLE = "NO"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	IPHOST := fmt.Sprintf("%s:%d", IP, HOST)
+
+	fmt.Printf("Servidor GATO conectado como: %s\n", IPHOST)
+
+	// Definir la dirección IP y el puerto del servidor
+	serverAddr, err := net.ResolveUDPAddr(PROTOCOLO, IPHOST)
+	if err != nil {
+		panic(err)
+	}
+
+	// Crear un objeto socket para el protocolo UDP
+	serverConn, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer serverConn.Close()
+
+	fmt.Printf("Servidor escuchando en %s\n", serverConn.LocalAddr().String())
+
+	for {
+		// Recibir un mensaje del cliente
+		buffer := make([]byte, 1024)
+		n, clientAddr, err := serverConn.ReadFromUDP(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		// Imprimir el mensaje recibido
+		fmt.Printf("Mensaje recibido de %s: %s\n", clientAddr.String(), buffer[:n])
+
+		// Enviar una respuesta al cliente
+		response := []byte("Hola, cliente!")
+		_, err = serverConn.WriteToUDP(response, clientAddr)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
